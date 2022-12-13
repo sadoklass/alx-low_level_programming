df7e101eca3c64555220aae7a023704e759bae7b
sadoklass <lassoued.sadok@gmail.com>
2022-12-13 10:22:25 -0800

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * return : always 0 (success)
+ */
+int main(void)
+
+{
+int i;
+char caract[] = "_putchar";
+for (i = 0 ; i < 8 ; i++)
+
+{
+
+_putchar(caract[i]);
+
+}
+_putchar('/n');
+return (0);
+}
