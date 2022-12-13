b9983fcb0ba6f6286601fb2a96249c6ce1972f8a
sadoklass <lassoued.sadok@gmail.com>
2022-12-13 11:54:37 -0800

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/**
- * main - entry point
- * return : always 0 (success)
- */
-int main(void)
 
+/**
+ * main - print _putchar
+ * main  return : 0
+ */
+
+int main(void)
 {
+
 int i;
 char caract[] = "_putchar";
+
 for (i = 0 ; i < 8 ; i++)
-
 {
-
 _putchar(caract[i]);
-
 }
 _putchar('\n');
 return (0);
