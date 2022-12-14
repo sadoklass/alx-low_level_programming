30363b0e82f2f74db852e50e36c4d7b984c5bbf6
sadoklass <lassoued.sadok@gmail.com>
2022-12-14 05:39:54 -0800

    task0correct1

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -17,15 +17,15 @@
  *
  * and if it is positive, negative, or zero
  *
- * Return: 0
+ *
  *
  */
 
-int main(void)
+void positive_or_negative(int n)
 
 {
 
-	int n;
+
 
 	srand(time(0));
 
@@ -33,17 +33,17 @@ int main(void)
 
 	if (n > 0)
 
-		printf("%i is positive\n", n);
+		printf("%d is positive\n", n);
 
 	else if (n < 0)
 
-		printf("%i is negative\n", n);
+		printf("%d is negative\n", n);
 
 	else
 
-		printf("%i is zero\n", n);
+		printf("%d is zero\n", n);
 
-	return (0);
+	
 
 
 
