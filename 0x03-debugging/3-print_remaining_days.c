ed12497008890fc95363920895edf2460620e64a
sadoklass <lassoued.sadok@gmail.com>
2022-12-14 10:15:03 -0800

    correct task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -35,7 +35,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day == 60)
+if (month > 12 || day > 31)
 {
 
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
