ff6f91ecdb89a26d421bf2b7c1ca29e2b8fa14a4
sadoklass <lassoued.sadok@gmail.com>
2022-12-19 14:20:24 -0800

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,16 +26,14 @@ while (s[i] != '\0')
 {
 if (s[i] == '-')
 sign *= -1;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
-																	while (s[i] >= '0' && s[i] <= '9')
-
-																	{
-
-																	numb = (s[i] - '0') * sign + numb * 10;
-																	i++;
-
-																	}
+while (s[i] >= '0' && s[i] <= '9')
+{
+numb = (s[i] - '0') * sign + numb * 10;
+i++;
+}
 																	break;
 																	}
 i++;
