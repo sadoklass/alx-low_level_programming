9375503767e5bba08ef14e6bff561c45c5e01319
sadoklass <lassoued.sadok@gmail.com>
2022-12-21 13:31:04 -0800

    correct task10 11 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,7 +13,6 @@
  */
 void print_buffer(char *b, int size)
 {
-
 int i = 0, j = 0, count = 0;
 while (j < size)
 {
@@ -21,26 +20,26 @@ printf("000000%02x: ", j);
 count = 0;
 while (count < 10)
 {
-																	if (i >= size)
-																	printf("  ");
-																	else
-																	printf("%02x", *(b + i));
-																	if (i % 2 != 0)
-																	printf(" ");
-																	i++;
-																	count++;
-																	}
+if (i >= size)
+printf("  ");
+else
+printf("%02x", *(b + i));
+if (i % 2 != 0)
+printf(" ");
+i++;
+count++;
+}
 i -= 10;
 count = 0;
-																	while (count < 10 && i < size)
-																	{
-																	printf("%c", *(b + i) > 31 && *(b + i) < 127 ? *(b + i) : '.');
-																	count++;
-																	i++;
+while (count < 10 && i < size)
+{
+printf("%c", *(b + i) > 31 && *(b + i) < 127 ? *(b + i) : '.');
+count++;
+i++;
 }
-																	if (j < size - 10)
-																	printf("\n");
-																	j += 10;
-																	}
+if (j < size - 10)
+printf("\n");
+j += 10;
+}
 printf("\n");
 }
