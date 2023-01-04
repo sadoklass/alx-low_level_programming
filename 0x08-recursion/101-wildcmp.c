a84a338c3711df8f18be657487205eff736b35e3
sadoklass <lassoued.sadok@gmail.com>
2023-01-04 12:33:03 -0800

    fix task8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,42 +1,28 @@
 #include "main.h"
 
-
-int search_other(char s, char *str, int *counter)
-
-{
-if (s == *str)
-return (1);
-else if (*str == '\0')
-return (0);
-else if (s == '*')
-return (0);
-else
-{
-*counter += 1;
-return (search_other(s, ++str, &*counter));
-}
-}
+/**
+ * wildcmp - Compare two strings allowing for wildcard char
+ *
+ * @s1: String being compared
+ *
+ * @s2: String being compared against
+ *
+ * Return: 1 if considered identical, 0 otherwise
+ *
+ */
 int wildcmp(char *s1, char *s2)
+
 {
-int counter;
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
-else if (*s1 == *s2)
-return (wildcmp(++s1, ++s2));
-else if (*s1 == '*')
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
 {
-counter = 0;
-search_other(*++s1, s2, &counter);
-s2 += counter;
-return (wildcmp(s1++, s2++));
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+return (1);
 }
-else if (*s2 == '*')
-{
-counter = 0;
-search_other(*++s2, s1, &counter);
-s1 += counter;
-return (wildcmp(s1++, s2++));
-}
-else
 return (0);
 }
