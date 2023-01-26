5f44bb9c9ef6651a852329d45f4a056f558c9630
sadoklass <lassoued.sadok@gmail.com>
2023-01-26 10:09:52 -0800

    fix task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,48 @@
 #include "lists.h"
 
-/**
- * _strlen - Returns the length of a string
- *
- * @s: String being evaluated
- *
- * Return: Length of string
- *
- */
+#include <string.h>
 
-int _strlen(const char *s)
-
-{
-unsigned int i = 0;
-while (*s++)
-++i;
-return (i);
-}
+#include <stdio.h>
 
 /**
- * add_node_end - Adds a node at the end of a list
+ * add_node_end - add a new node at the end of `list_t` list
  *
- * @head: Pointer to address of a list
+ * @head: double pointer to head
  *
- * @str: Pointer to string
+ * @str: string to duplicate into new node
  *
- * Return: Address of new element
- *
- * On error, NULL
+ * Return: Address of the new element or NULL if failed
  *
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 
 {
-list_t *new;
-list_t *old = *head;
-new = malloc(sizeof(list_t));
-if (!new)
-return (NULL);
-new->str = strdup(str);
-new->len = _strlen(str);
-new->next = NULL;
-if (!*head)
+list_t *current;
+list_t *new_node;
+int c;
+current = *head;
+while (current && current->next != NULL)
+current = current->next;
+for (c = 0; dup_str[c] != '\0'; c++)
+;
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
 {
-*head = new;
-return (*head);
+free(new_node);
+return (NULL);
 }
-while (old->next)
-old = old->next;
-old->next = new;
-return (new);
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->len = c;
+new_node->next = NULL;
+if (current)
+current->next = new_node;
+else
+*head = new_node;
+return (new_node);
 }
