045833b886a9bc46984519a1a5ac694131f64e45
sadoklass <lassoued.sadok@gmail.com>
2023-01-26 09:56:51 -0800

    fix task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,87 +1,54 @@
 #include "lists.h"
 
-#include <stdlib.h>
-
-#include <stdio.h>
-
-#include <stddef.h>
-
 /**
- * _strlen - gets length of the string
+ * _strlen - Returns the length of a string
  *
- * @s: string
+ * @s: String being evaluated
  *
- * Return: length of the string
+ * Return: Length of string
  *
  */
 
 int _strlen(const char *s)
 
 {
-int i;
-for (i = 0; s[i]; i++)
-;
+unsigned int i = 0;
+while (*s++)
+++i;
 return (i);
 }
-/**
- * _strdup - recreation of string duplicate function
- *
- * @src: source of string to duplicate
- *
- * Return: pointer to malloc'd space with copied string
- *
- */
-
-void *_strdup(const char *src)
-
-{
-int len, i;
-char *dest;
-len = _strlen(src);
-dest = malloc((len + 1) * sizeof(char));
-if (dest == NULL)
-return (NULL);
-for (i = 0; src[i]; i++)
-dest[i] = src[i];
-dest[i] = '\0';
-return (dest);
-}
 
 /**
- * add_node_end - add new nodes to the end of the list
+ * add_node_end - Adds a node at the end of a list
  *
- * @head: current place in the list
+ * @head: Pointer to address of a list
  *
- * @str: string to add to the head
+ * @str: Pointer to string
  *
- * Return: pointer to current position in list
+ * Return: Address of new element
+ *
+ * On error, NULL
  *
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 
 {
-list_t *new, *current;
-char *dupstr;
-if str == NULL)
-return (NULL);
-dupstr = _strdup(str);
-if (dupstr == NULL)
-return (NULL);
+list_t *new;
+list_t *old = *head;
 new = malloc(sizeof(list_t));
-if (new == NULL)
+if (!new)
 return (NULL);
-new->str = dupstr;
+new->str = strdup(str);
 new->len = _strlen(str);
 new->next = NULL;
-if (*head == NULL)
+if (!*head)
 {
 *head = new;
 return (*head);
 }
-current = *head;
-while (current->next != NULL)
-current = current->next;
-current->next = new;
-return (*head);
+while (old->next)
+old = old->next;
+old->next = new;
+return (new);
 }
