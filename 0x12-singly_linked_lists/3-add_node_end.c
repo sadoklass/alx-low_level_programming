8ae1c36655ce2e3e34cb4cace0ce753a8f44d38a
sadoklass <lassoued.sadok@gmail.com>
2023-01-26 10:43:22 -0800

    fix task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,59 +1,49 @@
-#include <stdio.h>
-
-#include <stdlib.h>
+#include "lists.h"
 
 #include <string.h>
 
-#include "lists.h"
-
 /**
- * add_node_end - adds a new node at the end of a list_t list
+ * add_node_end - add to end
  *
- * @head: pointer to pointer of first node of a list_t list
+ * @head: pointer to head
  *
- * @str: string to be included in node
+ * @str: input string
  *
- * Return: address of the new element or NULL if it fails
+ * Return: the address of the new element, or NULL if it failed
  *
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 
 {
-list_t *new;
-list_t *current;
-current = *head;
+
+list_t  *new, *ptr;
+unsigned int len = 0;
 new = malloc(sizeof(list_t));
-if (new == NULL)
-return (NULL);
-new->str = strdup(str);
-new->len = _strlen(str);
-new->next = NULL;
-if (*head == NULL)
-*head = new;
-else
+if (new)
 {
-while (current->next != NULL)
-current = current->next;
-current->next = new;
-}
+while (*(str + len))
+len++;
+new->str = strdup(str);
+if (new->str)
+{
+new->len = len;
+new->next = NULL;
+if (!*head)
+{
+*head = new;
 return (new);
 }
+																				ptr = *head;
+																				while (ptr->next)
+																				ptr = ptr->next;
+ptr->next = new;
+																				return (new);
 
-/**
- * _strlen - returns the length of a string
- *
- * @s: string to be evaluated
- *
- * Return: length of string
- *
- */
+																				}
 
-int _strlen(const char *s)
-
-{
-int i = 0;
-while (s[i] != '\0')
-i++;
-return (i);
+free(new);
+return (NULL);
+}
+return (NULL);
 }
